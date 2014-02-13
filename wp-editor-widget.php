bdee99c11c51d7b55b2b62b1ec5f10e5fd5657dc
Christian Foellmann <foellmann@foe-services.de>
2014-02-13 18:08:03 +0100

    changed textdomain and removed constant - http://ottopress.com/2013/language-packs-101-prepwork/

--- wp-editor-widget.php
+++ wp-editor-widget.php
@@ -6,14 +6,14 @@ Description: WP Editor Widget adds a WYSIWYG widget using the wp_editor().
 Author: David M&aring;rtensson, Odd Alice
 Version: 0.4.1
 Author URI: http://www.feedmeastraycat.net/
+Text Domain: wp-editor-widget
+Domain Path: /langs
 */
 
 
 
 // Setup actions
-add_action('init', array('WPEditorWidget', 'init'));
 add_action('admin_init', array('WPEditorWidget', 'admin_init'));
-add_action('plugins_loaded', array('WPEditorWidget', 'plugins_loaded'));
 add_action('widgets_admin_page', array('WPEditorWidget', 'widgets_admin_page'), 100);
 add_action('widgets_init', array('WPEditorWidget', 'widgets_init'));
 // Setup filters
@@ -27,29 +27,26 @@ add_filter('wp_editor_widget_content', 'do_shortcode', 11);
 
 
 
-/** 
+/**
  * WP Editor Widget singelton
  */
 class WPEditorWidget
 {
-	
+
 	/**
 	 * @var string
 	 */
 	const VERSION = "0.4.1";
-	
-	/**
-	 * @var string
-	 */
-	const TEXTDOMAIN = "wpeditorwidget";
-	
+
 	/**
 	 * Action: init
 	 */
-	public static function init()
-	{
+	public function __construct() {
+
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+
 	}
-	
+
 	/**
 	 * Action: admin_init
 	 */
@@ -57,27 +54,27 @@ class WPEditorWidget
 	{
 		wp_register_script('wp-editor-widget-js', plugins_url('assets/js/admin.js', __FILE__), array('jquery'), self::VERSION);
 		wp_enqueue_script('wp-editor-widget-js');
-		
+
 		wp_register_style('wp-editor-widget-css', plugins_url('assets/css/admin.css', __FILE__), array(), self::VERSION);
 		wp_enqueue_style('wp-editor-widget-css');
 	}
-	
+
 	/**
 	 * Action: plugins_loaded
 	 */
 	public static function plugins_loaded()
 	{
 		// Load translations
-		load_plugin_textdomain(self::TEXTDOMAIN, false, dirname(plugin_basename(__FILE__)).'/langs/');
+		load_plugin_textdomain('wp-editor-widget', false, dirname(plugin_basename(__FILE__)).'/langs/');
 	}
-	
+
 	/**
 	 * Action: widgets_admin_page
 	 */
 	public static function widgets_admin_page() {
 		?>
 		<div id="wp-editor-widget-container" style="display: none;">
-			<a class="close" href="javascript:WPEditorWidget.hideEditor();" title="<?php esc_attr_e('Close', self::TEXTDOMAIN) ?>"><span class="icon"></span></a>
+			<a class="close" href="javascript:WPEditorWidget.hideEditor();" title="<?php esc_attr_e('Close', 'wp-editor-widget') ?>"><span class="icon"></span></a>
 			<div class="editor">
 				<?php
 				$settings = array(
@@ -86,14 +83,14 @@ class WPEditorWidget
 				wp_editor('', 'wp-editor-widget', $settings);
 				?>
 				<p>
-					<a href="javascript:WPEditorWidget.updateWidgetAndCloseEditor(true);" class="button button-primary"><?php _e('Save and close', self::TEXTDOMAIN) ?></a>
+					<a href="javascript:WPEditorWidget.updateWidgetAndCloseEditor(true);" class="button button-primary"><?php _e('Save and close', 'wp-editor-widget') ?></a>
 				</p>
 			</div>
 		</div>
 		<div id="wp-editor-widget-backdrop" style="display: none;"></div>
 		<?php
 	}
-	
+
 	/**
 	 * Action: widgets_init
 	 */
@@ -101,7 +98,7 @@ class WPEditorWidget
 	{
 		register_widget('WP_Editor_Widget');
 	}
-	
+
 }
 
 
@@ -109,7 +106,7 @@ class WPEditorWidget
 /**
  * Adds WP_Editor_Widget widget.
  */
-class WP_Editor_Widget extends WP_Widget 
+class WP_Editor_Widget extends WP_Widget
 {
 
 	/**
@@ -118,8 +115,8 @@ class WP_Editor_Widget extends WP_Widget
 	public function __construct() {
 		parent::__construct(
 	 		'wp_editor_widget',
-			__('Rich text', WPEditorWidget::TEXTDOMAIN),
-			array('description' => __('Arbitrary text, HTML or rich text through the standard WordPress visual editor.', WPEditorWidget::TEXTDOMAIN))
+			__('Rich text', 'wp-editor-widget'),
+			array('description' => __('Arbitrary text, HTML or rich text through the standard WordPress visual editor.', 'wp-editor-widget'))
 		);
 	}
 
@@ -131,22 +128,22 @@ class WP_Editor_Widget extends WP_Widget
 	 * @param array $args Widget arguments.
 	 * @param array $instance Saved values from database.
 	 */
-	public function widget($args, $instance) 
+	public function widget($args, $instance)
 	{
 		extract( $args );
-		
+
 		$title = apply_filters('wp_editor_widget_title', $instance['title']);
 		$output_title = apply_filters('wp_editor_widget_output_title', $instance['output_title']);
 		$content = apply_filters('wp_editor_widget_content', $instance['content']);
-		
+
 		echo $before_widget;
-		
+
 		if ($output_title == "1" && !empty($title)) {
 			echo $before_title.$title.$after_title;
 		}
-		
+
 		echo $content;
-		
+
 		echo $after_widget;
 	}
 
@@ -157,38 +154,38 @@ class WP_Editor_Widget extends WP_Widget
 	 *
 	 * @param array $instance Previously saved values from database.
 	 */
-	public function form($instance) 
+	public function form($instance)
 	{
 		if (isset($instance['title'])) {
 			$title = $instance['title'];
 		}
 		else {
-			$title = __('New title', WPEditorWidget::TEXTDOMAIN);
+			$title = __('New title', 'wp-editor-widget');
 		}
-		
+
 		if (isset($instance['content'])) {
 			$content = $instance['content'];
 		}
 		else {
 			$content = "";
 		}
-		
+
 		$output_title = (isset($instance['output_title']) && $instance['output_title'] == "1" ? true:false);
 		?>
 		<input type="hidden" id="<?php echo $this->get_field_id('content'); ?>" name="<?php echo $this->get_field_name('content'); ?>" value="<?php echo esc_attr($content); ?>">
 		<p>
-			<label for="<?php echo $this->get_field_name('title'); ?>"><?php _e('Title', WPEditorWidget::TEXTDOMAIN); ?>:</label> 
+			<label for="<?php echo $this->get_field_name('title'); ?>"><?php _e('Title', 'wp-editor-widget'); ?>:</label>
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
 		</p>
 		<p>
-			<a href="javascript:WPEditorWidget.showEditor('<?php echo $this->get_field_id('content'); ?>');"><?php _e('Edit content', WPEditorWidget::TEXTDOMAIN) ?></a>
+			<a href="javascript:WPEditorWidget.showEditor('<?php echo $this->get_field_id('content'); ?>');"><?php _e('Edit content', 'wp-editor-widget') ?></a>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id('output_title'); ?>">
-				<input type="checkbox" id="<?php echo $this->get_field_id('output_title'); ?>" name="<?php echo $this->get_field_name('output_title'); ?>" value="1" <?php checked($output_title, true) ?>> <?php _e('Output title', WPEditorWidget::TEXTDOMAIN); ?>
-			</label> 
+				<input type="checkbox" id="<?php echo $this->get_field_id('output_title'); ?>" name="<?php echo $this->get_field_name('output_title'); ?>" value="1" <?php checked($output_title, true) ?>> <?php _e('Output title', 'wp-editor-widget'); ?>
+			</label>
 		</p>
-		<?php 
+		<?php
 	}
 
 	/**
@@ -201,10 +198,10 @@ class WP_Editor_Widget extends WP_Widget
 	 *
 	 * @return array Updated safe values to be saved.
 	 */
-	public function update($new_instance, $old_instance) 
+	public function update($new_instance, $old_instance)
 	{
 		$instance = array();
-		
+
 		$instance['title'] = (!empty($new_instance['title']) ? strip_tags( $new_instance['title']):'');
 		$instance['content'] = (!empty($new_instance['content']) ? $new_instance['content']:'');
 		$instance['output_title'] = (isset($new_instance['output_title']) && $new_instance['output_title'] == "1" ? 1:0);
@@ -212,4 +209,4 @@ class WP_Editor_Widget extends WP_Widget
 		return $instance;
 	}
 
-} 
+}
