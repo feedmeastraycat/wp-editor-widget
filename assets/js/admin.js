6dfc2698d88e10737dca6dba4a7e6fc8365feb59
David Mårtensson <david@oddalice.se>
2014-04-16 22:35:42 +0200

    Functionality for customize.php

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,6 +7,16 @@ WPEditorWidget = {
 	 * @var string
 	 */
 	currentContentId: '',
+	
+	/**
+	 * @var string
+	 */
+	 currentEditorPage: '',
+	 
+	 /**
+	  * @var int
+	  */
+	 wpFullOverlayOriginalZIndex: 0,
 
 	/**
 	 * Show the editor
@@ -17,6 +27,12 @@ WPEditorWidget = {
 		jQuery('#wp-editor-widget-container').show();
 		
 		this.currentContentId = contentId;
+		this.currentEditorPage = ( jQuery('body').hasClass('wp-customizer') ? 'wp-customizer':'wp-widgets');
+		
+		if (this.currentEditorPage == "wp-customizer") {
+			this.wpFullOverlayOriginalZIndex = parseInt(jQuery('.wp-full-overlay').css('zIndex'));
+			jQuery('.wp-full-overlay').css({ zIndex: 49000 });
+		}
 		
 		this.setEditorContent(contentId);
 	},
@@ -27,6 +43,10 @@ WPEditorWidget = {
 	hideEditor: function() {
 		jQuery('#wp-editor-widget-backdrop').hide();
 		jQuery('#wp-editor-widget-container').hide();
+		
+		if (this.currentEditorPage == "wp-customizer") {
+			jQuery('.wp-full-overlay').css({ zIndex: this.wpFullOverlayOriginalZIndex });
+		}
 	},
 	
 	/**
@@ -47,13 +67,26 @@ WPEditorWidget = {
 	 */
 	updateWidgetAndCloseEditor: function() {
 		var editor = tinyMCE.EditorManager.get('wp-editor-widget');
+
 		if (typeof editor == "undefined") {
 			jQuery('#'+ this.currentContentId).val(jQuery('#wp-editor-widget').val());
 		}
 		else {
 			jQuery('#'+ this.currentContentId).val(editor.getContent());
 		}
-		wpWidgets.save(jQuery('#'+ this.currentContentId).closest('div.widget'), 0, 1, 0);
+		
+		// customize.php
+		if (this.currentEditorPage == "wp-customizer") {
+			var widget_id = jQuery('#'+ this.currentContentId).closest('div.form').find('input.widget-id').val();
+			var widget_form_control = wp.customize.Widgets.getWidgetFormControlForWidget( widget_id )
+			widget_form_control.updateWidget();
+		}
+		
+		// widgets.php
+		else {
+			wpWidgets.save(jQuery('#'+ this.currentContentId).closest('div.widget'), 0, 1, 0);	
+		}
+		
 		this.hideEditor();
 	}
 	
